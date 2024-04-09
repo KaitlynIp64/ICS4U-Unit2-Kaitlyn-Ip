6c949dd143245b975b2ac12c5c3f6395debf1d14
Mr Coxall <mr.coxall@mths.ca>
2024-04-09 14:02:56 +0000

    stuff

---
+++ Unit2-04/Main.java
@@ -0,0 +1,51 @@
+/*
+ * This is a program that pops stack.
+ *
+ * @author  Kaitlyn Ip
+ * @version 1.0
+ * @since   2024-01-01
+ */
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<String> fruits = new Stack<>();
+        fruits.push("apple");
+        fruits.push("orange");
+        fruits.push("watermelon");
+        System.out.println("cleared stack");
+        fruits.clear();
+        System.out.println("popped: " + fruits.pop());
+        System.out.println("fruits: " + fruits);
+        System.out.println("length: " + fruits.size());
+        System.out.println("is empty: " + fruits.isEmpty());
+        System.out.println();
+
+        Stack<String> colours = new Stack<>();
+        colours.push("red");
+        colours.push("blue");
+        colours.push("purple");
+        System.out.println("length: " + colours.size());
+        System.out.println("popped: " + colours.pop());
+        System.out.println("colours: " + colours);
+        System.out.println("is empty: " + colours.isEmpty());
+        System.out.println();
+
+        Stack<String> animals = new Stack<>();
+        animals.push("dog");
+        animals.push("otter");
+        animals.push("duck");
+        animals.push("cat");
+        animals.push("bunny");
+        System.out.println("popped: " + animals.pop());
+        System.out.println("popped: " + animals.pop());
+        System.out.println("popped: " + animals.pop());
+        System.out.println("peak last item: " + animals.peek());
+        System.out.println("animals: " + animals);
+        System.out.println("length: " + animals.size());
+
+        System.out.println("\nDone.");
+    }
+}
+
