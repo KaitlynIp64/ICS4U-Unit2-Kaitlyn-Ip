d74fff8f2e16343ccd195b1e47b2b93bee25decc
Kaitlyn Ip <Kaitlyn Ip>
2024-04-16 08:51:08 -0400

    added 2-06 ts

---
+++ Unit2-06/Airplane.ts
@@ -0,0 +1,28 @@
+/*
+* This program is the standard airplane class.
+*
+* @author  Kaitlyn Ip
+* @version 1.0
+* @since   2024-03-25
+*/
+
+
+// This is the airplane class.
+export class Airplane {
+    /** The internal variable that stores speed. */
+    private speed: number
+
+    // Constructor.
+    public Airplane() {
+    }
+
+    // Set an airplane speed.
+    public setSpeed(speed: number) {
+        this.speed = speed
+    }
+
+    // Get speed.
+    public getSpeed(): number {
+        return this.speed
+    }
+}
---
+++ Unit2-06/Jet.ts
@@ -0,0 +1,32 @@
+/*
+* This program is the standard jet class.
+*
+* @author  Kaitlyn Ip
+* @version 1.0
+* @since   2024-03-25
+*/
+
+import { Airplane } from "./Airplane"
+
+/**
+* This is the jet class.
+*/
+export class Jet extends Airplane {
+    /** Jet speed change multiplier. */
+    private MULTIPLIER: number = 2
+
+    // Constructor.
+    public Jet() {
+        // super()
+    }
+
+    // Set jet speed.
+    public setSpeed(speed: number) {
+        super.setSpeed(speed * this.MULTIPLIER)
+    }
+
+    // Double speed.
+    public accelerate() {
+        super.setSpeed(super.getSpeed() * 2)
+    }
+}
