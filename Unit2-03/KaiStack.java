2ca623633659a9aa558ca85a14ed5bdc27625c0b
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2024-05-26 22:42:39 +0000

    init

---
+++ Unit2-03/KaiStack.java
@@ -0,0 +1,81 @@
+/*
+* This program calculates the energy released when mass is converted to energy.
+*
+* @author  Kaitlyn Ip
+* @version 1.0
+* @since   2024-03-25
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* This is my stack.
+*/
+public class KaiStack {
+    /** The internal variable that stores stack. */
+    private List<String> strstack = new ArrayList<String>();
+
+    /**
+    * Push a string to end of stack.
+    *
+    * @param input user input string
+    */
+    public void push(String input) {
+        strstack.add(input);
+    }
+
+    /**
+    * Get if stack is empty.
+    *
+    * @return get if stack is empty
+    */
+    public boolean isEmpty() {
+        return strstack.size() == 0;
+    }
+
+    /**
+    * Get stack size.
+    *
+    * @return get length
+    */
+    public int size() {
+        return strstack.size();
+    }
+
+    /**
+    * Show all items on stack.
+    *
+    * @return the full stack
+    */
+    public String show() {
+        String values = "";
+        if (strstack != null) {
+            for (int counter = 0; counter < strstack.size(); counter++) {
+                values = values + strstack.get(counter) + ", ";
+            }
+            if (strstack.size() > 0) {
+                values = values.substring(0, values.length() - 2);
+            }
+        } else {
+            values = "stack is empty";
+        }
+        return values;
+    }
+
+    /**
+    * Pop last item from stack.
+    *
+    * @return the popped item
+    */
+    public String pop() {
+        String removed = "";
+        if (strstack.size() > 0) {
+            removed = strstack.get(strstack.size() - 1);
+            strstack.remove(strstack.size() - 1);
+        } else {
+            removed = "nothing to remove";
+        }
+        return removed;
+    }
+}
